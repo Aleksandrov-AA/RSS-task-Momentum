41804e79d8eedcccdb2a7810089218d4b4df3a8c
Aleksandrov-AA <sana115@mail.ru>
2022-08-15 18:22:30 +0300

    feat: add showGreeting, but Storage(save name) don't work (need to fix later)

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ function showTime() {
 
     time.textContent = currentTime;
     showDate();
+    showGreeting();
     setTimeout(showTime, 1000);    
 }
 
@@ -24,8 +25,7 @@ function showDate() {
     // подробнее про формат отображения по ссылке ниже
     // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Date/toLocaleDateString
 
-    dateText.textContent = currentDate;
-    console.log(currentDate);
+    dateText.textContent = currentDate;    
 }
 
 showDate();
@@ -34,11 +34,54 @@ showDate();
 // 2. Приветствие
 
 function getTimeOfDay() {
-
+    const date = new Date();
+    const hours = date.getHours();
+    
+    if (hours >= 0 && hours <= 3) {
+        return 'night';
+    } else if (hours >= 4 && hours <= 11) {
+        return 'morning';
+    } else if (hours >= 12 && hours <= 16) {
+        return 'afternoon';
+    } else if (hours >= 17 && hours <= 23) {
+        return 'evening'
+    }
+    // стандартная разбивка по 6 часов, но не совсем логичная по времени суток:
+    // if (hours >= 0 && hours < 6) {
+    //     return 'night';
+    // } else if (hours >= 6 && hours < 12) {
+    //     return 'morning';
+    // } else if (hours >= 12 && hours < 18) {
+    //     return 'day';
+    // } else if (hours >= 18 && hours < 24) {
+    //     return 'evening'
+    // }
 }
 
 function showGreeting() {
-    
+    const timeOfDay = getTimeOfDay();
+    const greetingText = `Good ${timeOfDay}`;
+    const greeting = document.querySelector('.greeting');
+
+    greeting.textContent = greetingText;
 }
 
+// const name5 = name.value;
+// console.log(name5);
 
+// Пока не сохраняется значение имени, не могу понять почему. Надо починить позже!
+
+function setLocalStorage() {
+    localStorage.setItem('name', name.value);
+}
+window.addEventListener('beforeunload', setLocalStorage)
+
+function getLocalStorage() {
+    if(localStorage.getItem('name')) {
+        name.value = localStorage.getItem('name');
+    }
+}
+window.addEventListener('load', getLocalStorage)
+
+
+// 3. Приветствие
